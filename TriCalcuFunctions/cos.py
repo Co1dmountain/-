17ef5edac258246fa85455ba71163d1b4085f76a
Co1dmountain <57553584+Co1dmountain@users.noreply.github.com>
2022-04-24 16:57:02 +0800

    Add files via upload

--- TriCalcuFunctions/cos.py
+++ TriCalcuFunctions/cos.py
@@ -6,14 +6,15 @@ def factorial(n):#求阶乘
 		result *= i
 	return result
 
-def sin(radian):
-	#输入角度值，返回角度值对应的sin值,精度到计算50次为止
+def cos(radian):
+	#输入角度值，返回角度值对应的cos值,精度到50次为止
 	#radian = x/180*pi#将角度转化为弧度值
-	result = radian
+	result = 1
 	for j in range(1, 50):#计算50次幂函数
 		if j%2 == 0:
-			result += (radian**(2*j+1))/factorial(2*j+1)
+			result += (radian**(2*j))/factorial(2*j)
 		else:
-			result -= (radian**(2*j+1))/factorial(2*j+1)
+			result -= (radian**(2*j))/factorial(2*j)
 	return round(result,3)#返回百分位精度的结果
 
+#print(cos(60))
